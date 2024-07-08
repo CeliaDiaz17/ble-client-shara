b1b17b8f5453d634cb477a9d99afddd8adb0fe71
CeliaDiaz17 <64320930+CeliaDiaz17@users.noreply.github.com>
2024-07-08 18:39:55 +0200

    ble server python
    this code uses repo from pycon

---
+++ pycon_ble_demo-master/main.py
@@ -0,0 +1,27 @@
+import multiprocessing
+import queue
+import time
+
+import dbus
+
+from demo.ble_process import BLEProcess
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+
+def main():
+    output_queue = multiprocessing.Queue()
+
+    ble_process = BLEProcess(output_queue)
+    ble_process.start()
+
+    while True:
+        try:
+            curr_value = output_queue.get(timeout=1)
+            print(f"Value written to Characteristic with UUID {curr_value['uuid']}: {curr_value['value']}")
+        except queue.Empty:
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
