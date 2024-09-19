d0f9577ef71fee6b2424d30cebbe5fb4a6c173d2
CeliaDiaz17 <Celia.diaz3@alu.uclm.es>
2024-09-19 14:00:43 +0200

    gitignore and new main

---
+++ main.py
@@ -0,0 +1,5 @@
+'''
+Módulo principal del programa. Este se ejecutará en el SHARA (cliente) y se encargara de 
+coordinar la conectividad con los dispositivos BLE, y usará el resto de módulos del programa 
+para generar y evaluar preguntas, y reproducir los resultados. 
+'''
