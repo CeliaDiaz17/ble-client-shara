14cd56b847df562173f53ebfae1429c41abee4b3
CeliaDiaz17 <Celia.diaz3@alu.uclm.es>
2024-11-19 18:46:27 +0100

    integrated to main

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ from services.data_evaluation import  get_corrects, calculate_percent
 
 #TODO: Implementar el pasive_scaning en el main
 
-def main():
+async def main():
     quiz = Quiz()
     speaker = Speaker()
     scanner = PassiveBluetoothScanner()
-    #ble_manager = BleManager()
+
+    await scanner.handshake()
     
     #TODO: Sustituir esto por la llamada a la API para que genere un quiz nuevo cada vez    
     
@@ -52,6 +53,7 @@ def main():
 
     #asyncio.run(handle_ble_cycle())
     print("INICIO DEL SISTEMA")   
+    speaker.speak("Bienvenidos a todos y a todas! Vamos a comenzar un cuestionario de 4 preguntas sobre el temario. Estad atentos. Tendréis 15 segundos para contestar cada pregunta. ¡Suerte!")
     for question_number in range(1, 5):
 
         statement = get_statement(quiz, question_number)
@@ -59,11 +61,10 @@ def main():
         
         speaker.speak(statement)
         speaker.speak(options)
+        speaker.speak("Desde ahora, teneis 15 segundos para responder")
         time.sleep(5) #tiempo para que los alumnos respondan
         
-        device_responses = asyncio.run(scanner.scan_devices())
-        
-        #device_responses = asyncio.run(scan_responses())
+        device_responses = await scanner.retrieve_device_data()
         
         #hasta que tengamos la evaluacion
         print(f"Obtained responses: {device_responses}")
@@ -90,24 +91,24 @@ def main():
     print("FIN DEL SISTEMA")  
 
 
-if __name__ == "__main__":
-    main()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
 
 
 #CODIGO AUXILIAR
 
-    '''
-    MICKY HERRAMIENTA QUE USAREMOS MAS TARDE
+'''
+MICKY HERRAMIENTA QUE USAREMOS MAS TARDE
+
+json_file_path = "files/quiz.json"
+
+str_quiz = quiz.create_quiz(quiz.quiz_prompt)
+
+dict_quiz = json.loads(str_quiz)
     
-    json_file_path = "files/quiz.json"
-    
-    str_quiz = quiz.create_quiz(quiz.quiz_prompt)
-    
-    dict_quiz = json.loads(str_quiz)
-        
-    with open(json_file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(dict_quiz, json_file, ensure_ascii=False, indent=4)
-    '''
+with open(json_file_path, 'w', encoding='utf-8') as json_file:
+    json.dump(dict_quiz, json_file, ensure_ascii=False, indent=4)
+'''
 
 
